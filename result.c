dd0f2fa4a543f1abdbe7aa4b2d177ce7a02a3271
Andres U <urizandres@estudiantec.cr>
2025-06-12 13:45:02 -0600

    cleanup

--- result.c
+++
@@ -1,12 +0,0 @@
-Declared x as int succesfully 
-Declared color succesfully 
-t = 0 
-t = 360 
-t = 5 
-x = 320 
-y = 240 
-color set 
-wait = 1 
-Executing loop 
-Executing frame 
-Program executed without errors 
